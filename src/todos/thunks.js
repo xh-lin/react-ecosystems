b75f42a78da639c03aad2ed1969be909b372f493
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2022-01-17 13:24:21 -0500

    Async thunks

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,3 +1,18 @@
+import { loadTodosInProgress, loadTodosSuccess, loadTodosFailure } from "./actions";
+
+export const loadTodos = () => async (dispatch, getState) => {
+    try {
+        dispatch(loadTodosInProgress());
+        const response = await fetch('http://localhost:8080/todos');
+        const todos = await response.json();
+
+        dispatch(loadTodosSuccess(todos));
+    } catch (err) {
+        dispatch(loadTodosError());
+        dispatch(displayAlert(err));
+    }
+};
+
 export const displayAlert = text => () => {
-    alert(`You clicked on: ${text}`);
+    alert(text);
 }
