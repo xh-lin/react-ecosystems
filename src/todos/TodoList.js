d45fff3df24126226af1c759b9268eb8252503a3
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2022-01-17 13:36:03 -0500

    Adding another reducer

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import TodoListItem from './TodoListItem';
 import NewTodoForm from './NewTodoForm';
+import { loadTodos } from './thunks';
 import { removeTodo, completeTodo } from './actions';
 import './TodoList.css';
 
-const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => (
-    <div className="list-wrapper">
-        <NewTodoForm />
-        {todos.map(todo => 
-            <TodoListItem 
-                todo={todo} 
-                onRemovePressed={onRemovePressed}
-                onCompletedPressed={onCompletedPressed} />)}
-    </div>
-);
+const TodoList = ({ todos = [], isLoading, onRemovePressed, onCompletedPressed, startLoadingTodos }) => {
+    useEffect(() => {
+        startLoadingTodos();
+    }, []);
+
+    const loadingMessage = <div>Loading todos...</div>;
+    const content = (
+        <div className="list-wrapper">
+            <NewTodoForm />
+            {todos.map(todo => 
+                <TodoListItem 
+                    todo={todo} 
+                    onRemovePressed={onRemovePressed}
+                    onCompletedPressed={onCompletedPressed} />)}
+        </div>
+    );
+    return isLoading ? loadingMessage : content;
+};
 
 const mapStateToProp = state => ({
     todos: state.todos,
+    isLoading: state.isLoading,
 })
 
 const mapDispatchToProps = dispatch => ({
     onRemovePressed: text => dispatch(removeTodo(text)),
     onCompletedPressed: text => dispatch(completeTodo(text)),
+    startLoadingTodos: () => dispatch(loadTodos()),
 })
 
 export default connect(mapStateToProp, mapDispatchToProps)(TodoList);
--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -1,4 +1,24 @@
-import { CREATE_TODO, REMOVE_TODO, COMPLETE_TODO } from './actions';
+import { 
+    CREATE_TODO, REMOVE_TODO, COMPLETE_TODO, 
+    LOAD_TODOS_IN_PROGRESS, LOAD_TODOS_SUCCESS, LOAD_TODOS_FAILURE 
+} from './actions';
+
+export const isLoading = (state = false, action) => {
+    const { type } = action;
+
+    switch (type) {
+        case LOAD_TODOS_IN_PROGRESS: {
+            return true;   
+        }
+        case LOAD_TODOS_SUCCESS:
+        case LOAD_TODOS_FAILURE: {
+            return false;
+        }
+        default: {
+            return state;
+        }
+    }
+}
 
 export const todos = (state = [], action) => {
     const { type, payload } = action;
